66ce9cd2ea600b52b09130b49d2da2a5ff13a928
Derya Antonelli <derya.antonelli@gmail.com>
2021-05-04 13:21:41 +0100

    Lift up state to parent component

--- src/index.js
+++ src/index.js
@@ -1,21 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function Square() {
-  return <button className="square">{/* TODO */}</button>;
+/**
+ * TODO
+ * RE-SET HISTORY WHEN HISTORY BUTTON CLICKED
+ * https://reactjs.org/tutorial/tutorial.html#why-immutability-is-important
+ *
+ */
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
-function Board() {
-  const [status] = useState("Next player: X");
+function Square({ value, onClick }) {
+  return (
+    <button className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
+}
 
+function Board({ squares, onClick }) {
   function renderSquare(i) {
-    return <Square />;
+    return <Square value={squares[i]} onClick={() => onClick(i)} />;
   }
 
   return (
     <div>
-      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -36,14 +64,64 @@ function Board() {
 }
 
 function Game() {
+  const [history, setHistory] = useState([
+    {
+      squares: Array(9).fill(null),
+    },
+  ]);
+
+  const [xIsNext, setXIsNext] = useState(true);
+  const [status, setStatus] = useState("Next player is X");
+  const [stepNumber, setStepNumber] = useState(history.length);
+
+  function handleClick(i) {
+    const timeline = history.slice(0, stepNumber + 1);
+    const current = timeline[timeline.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = xIsNext ? "X" : "O";
+    setHistory(timeline.concat([{ squares: squares }]));
+    setStepNumber(timeline.length);
+    setXIsNext(!xIsNext);
+  }
+
+
+  function jumpTo(step) {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
+  }
+
+  const moves = history.map((step, move) => {
+    const desc = move ? "Go to move #" + move : "Go to game start";
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    );
+  });
+
+  useEffect(() => {
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+    let currStatus;
+    if (winner) {
+      currStatus = "Winner: " + winner;
+    } else {
+      currStatus = "Next player: " + (xIsNext ? "X" : "O");
+    }
+    setStatus(currStatus);
+  }, [history,xIsNext]);
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board squares={history[history.length -1].squares} onClick={(i) => handleClick(i)} />
       </div>
       <div className="game-info">
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
+        <div>{status}</div>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
