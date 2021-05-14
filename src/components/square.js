e55686a0a7e743c358da413700d66d76e19158e1
Derya Antonelli <derya.antonelli@gmail.com>
2021-05-14 13:23:45 +0100

    Style square

--- src/components/square.js
+++ src/components/square.js
@@ -1,15 +1,48 @@
-export default function Square({ value, onClick, style }) {
-  const highlightStyle = { backgroundColor: "yellow" };
-  const boldStyle = { color: "red" };
-  const standartStyle = { color: "black" };
-  const { bold, highlight } = style;
+import styled, {keyframes} from "styled-components";
+
+const appear = keyframes`
+  0% {
+    transform: scale(0.3);
+  }
+  100% {
+    transform: scale(1);
+  }
+`;
+
+
+const Tile = styled.button`
+  float: left;
+  line-height: 34px;
+  width: 154px;
+  height: 154px;
+  margin-right: -1px;
+  margin-top: -1px;
+  padding: 0;
+  text-align: center;
+  border: none;
+  border-right: ${({ position }) =>
+    position !== 2 && position !== 5 && position !== 8 && "10px solid #e4ff03"};
+  border-bottom: ${({ position }) =>
+    position !== 6 && position !== 7 && position !== 8 && "10px solid #e4ff03"};
+  background-color: ${({ style }) =>
+    (style.highlight && "yellow") || "#642b9d"};
+  font-size: 8rem;
+  color: ${({ style }) => (style.bold && "#FF5F00") || "#28C31A"};
+  -webkit-text-stroke: 2px black;
+`;
+
+const Move = styled.span`
+  display: inline-block;
+  animation-name: ${({ value }) => value && appear};
+  animation-duration: .2s;
+  animation-timing-function: cubic-bezier(0.5, 1.8, 0.1, 1.1);
+`;
+
+export default function Square({ value, onClick, style, position }) {
+  console.log(value)
   return (
-    <button
-      className="square"
-      onClick={onClick}
-      style={bold ? boldStyle : highlight ? highlightStyle : standartStyle}
-    >
-      {value}
-    </button>
+    <Tile onClick={onClick} position={position} style={style}>
+      <Move value={value} style={style}>{value}</Move>
+    </Tile>
   );
 }
